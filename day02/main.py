c71dae8edcdf5d3d2b5454cb3f23d6925c3244c1
RB <rohanchess2001@gmail.com>
2021-12-02 22:39:16 +0530

    Files updated for day 2

--- day02/main.py
+++ day02/main.py
@@ -1,18 +1,21 @@
 with open('data.txt', 'r') as file:
     my_list = file.readlines()
-    total1 = 0
-    total2 = 0
+    aim = 0
+    forward = 0
+    down = 0
+    down2 = 0
     for item in my_list:
-        password = item.split(': ')[1]
-        policy = item.split(': ')[0]
-        policy_condition = policy.split(' ')[0]
-        letter = policy.split(' ')[1]
-        count = password.count(letter)
-        if int(policy_condition.split('-')[0]) <= count <= int(policy_condition.split('-')[1]):
-            total1 += 1
-        first_letter = password[int(policy_condition.split('-')[0]) - 1]
-        second_letter = password[int(policy_condition.split('-')[1]) - 1]
-        letters_combined = first_letter + second_letter
-        if letters_combined.count(letter) == 1:
-            total2 += 1
-    print(f'Answer for problem 1:{total1}\nAnswer for problem 2:{total2}')
+        command = item.split(' ')[0]
+        distance = int(item.split(' ')[1])
+        if command == 'forward':
+            forward += distance
+            down2 += distance * aim
+        elif command == 'down':
+            down += distance
+            aim += distance
+        else:
+            down -= distance
+            aim -= distance
+    ans1 = forward * down
+    ans2 = forward * down2
+    print(f'Answer for problem 1:{ans1}\nAnswer for problem 2:{ans2}')
